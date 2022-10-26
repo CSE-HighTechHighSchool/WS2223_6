cb4a47b490ad36cbb2471919156bb884da4b7bac
Dzlotnick <daniel.zlotnick5@gmail.com>
2022-10-26 10:47:54 -0400

    Formatting and Cards
    fixed formatting for html/css started creating famous people cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let count = 0;
 document.querySelector('.card-button').addEventListener("click", () => {
-    if (count % 2 === 0){
+    if (count % 2 === 0) {
         document.querySelector('.card-button').style.setProperty("--flip", "rotate(90deg)");
     }
     else {
