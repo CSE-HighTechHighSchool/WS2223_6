574efc7eafe5a824cc0da0c1668846799756bc54
Dzlotnick <daniel.zlotnick5@gmail.com>
2022-11-02 08:00:35 -0400

    Created on-scroll animations for the timeline

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,11 @@ document.querySelectorAll('.card-button').forEach((button) => {
         count++;
     })
 });
+AOS.init({
+  offset: 400, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000 // values from 0 to 3000, with step 50ms
+});
+
+  
 
