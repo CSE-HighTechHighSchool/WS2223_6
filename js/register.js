2efe6ba9ae17c21986369721236d03feba34f501
SeanProce <seanproce@gmail.com>
2023-01-04 08:56:35 -0500

    Register html + css pages

--- js/register.js
+++ js/register.js
@@ -28,3 +28,95 @@ const auth = getAuth();
 
 // Return instance of the app's FRD
 const db = getDatabase(app);
+
+// ---------------- Register New Uswer --------------------------------//
+document.getElementById('submitData').onclick = function () {
+  const firstName = document.getElementById('firstName').value
+  const lastName = document.getElementById('lastName').value
+  const email = document.getElementById('userEmail').value
+
+  //Firebase requires a password of at least 6 characters
+  const password = document.getElementById('userPass').value
+
+  //Validate user inputs
+  if (!validation(firstName, lastName, email, password)) {
+    return
+  }
+
+
+  //Create new app user
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // alert("User created successfully!")
+    //Add user account info to realtime database
+    //set:  creates new reference or replace existing reference
+    //each new user iwll be placed under the users node
+    set(ref(db, 'users/' + user.uid + '/accountInfo'),{
+      uid: user.uid,
+      email: email,
+      password: encryptPass(password),  
+      firstname: firstName,
+      lastname: lastName
+    })
+    .then(()=>{
+      //Data saved successfully
+      alert('User created successfully!')
+      window.location= 'tracker.html'
+    })
+    .catch((error)=>{
+      alert(error)
+    })
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+    // ..
+  });
+}
+
+// --------------- Check for null, empty ("") or all spaces only ------------//
+function isEmptyorSpaces(str) {
+  return str === null || str.match(/^ *$/) !== null
+}
+
+// ---------------------- Validate Registration Data -----------------------//
+function validation(firstName, lastName, email, password) {
+  let fNameRegex = /^[a-zA-Z]+$/
+  let lNameRegex = /^[a-zA-Z]+$/
+  let emailRegex = /^\w+@(gmail|ctemc|yahoo){1}\.(com|org){1}$/
+
+  if (isEmptyorSpaces(firstName) || isEmptyorSpaces(lastName) || isEmptyorSpaces(email) || isEmptyorSpaces(password)) {
+    alert("please complete all fields.")
+    return false;
+  }
+  if(!fNameRegex.test(firstName)){
+    alert("The first name should only contain letters.")
+    return false
+  }
+  if(!lNameRegex.test(lastName)){
+    alert("The last name should only contain letters.")
+    return false
+  }
+  if(!emailRegex.test(email)){
+    alert("Please enter a valid email.")
+    return false
+  }
+  return true
+}
+
+// --------------- Password Encryption -------------------------------------//
+
+function encryptPass(password){
+  let encrypted = CryptoJS.AES.encrypt(password, password)
+  return encrypted.toString()
+}
+
+function decryptPass(password){
+  let decrypted = CryptoJS.AES.decrypt(password, password)
+  return decrypted.toString()
+}
+
